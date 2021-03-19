b4ef6fa861667bdc9c2f59d0159c69d0596e8034
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-03-19 21:43:39 +0100

    First version is right here

--- src/main/java/de/melanx/jea/EventHandler.java
+++ src/main/java/de/melanx/jea/EventHandler.java
@@ -1,17 +1,23 @@
 package de.melanx.jea;
 
+import de.melanx.jea.config.JeaConfig;
 import de.melanx.jea.render.FakeClientPlayer;
+import io.github.noeppi_noeppi.libx.event.ConfigLoadedEvent;
 import io.github.noeppi_noeppi.libx.event.DatapacksReloadedEvent;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderNameplateEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class EventHandler {
+    
+    private boolean needsResyncAdvancements = false;
     
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
@@ -26,8 +32,25 @@ public class EventHandler {
     }
     
     @SubscribeEvent
+    public void serverTick(TickEvent.WorldTickEvent event) {
+        // Reason that this is not a ServerTickEvent is that we need a MinecraftServer instance.
+        if (this.needsResyncAdvancements && event.world instanceof ServerWorld) {
+            this.needsResyncAdvancements = false;
+            JustEnoughAdvancements.getNetwork().syncAdvancements(event.world.getServer());
+        }
+    }
+    
+    @SubscribeEvent
+    public void configReload(ConfigLoadedEvent event) {
+        if (event.getConfigClass() == JeaConfig.class && event.getReason() == ConfigLoadedEvent.LoadReason.RELOAD) {
+            this.needsResyncAdvancements = true;
+        }
+    }
+    
+    @SubscribeEvent
     @OnlyIn(Dist.CLIENT)
     public void renderNameplate(RenderNameplateEvent event) {
+        // Fake steve should not have a name plate
         if (event.getEntity() instanceof FakeClientPlayer) {
             if (event.isCancelable()) {
                 event.setCanceled(true);
--- src/main/java/de/melanx/jea/JustEnoughAdvancements.java
+++ src/main/java/de/melanx/jea/JustEnoughAdvancements.java
@@ -2,13 +2,9 @@ package de.melanx.jea;
 
 import de.melanx.jea.api.CriterionSerializer;
 import de.melanx.jea.api.JeaRegistries;
-import de.melanx.jea.api.client.Jea;
 import de.melanx.jea.config.JeaConfig;
 import de.melanx.jea.network.JustEnoughNetwork;
-import de.melanx.jea.plugins.vanilla.VanillaCriteriaIds;
-import de.melanx.jea.plugins.vanilla.VanillaCriteriaSerializers;
-import de.melanx.jea.plugins.vanilla.criteria.*;
-import de.melanx.jea.plugins.vanilla.special.RideStriderInfo;
+import de.melanx.jea.plugins.vanilla.VanillaPlugin;
 import de.melanx.jea.render.SpecialModels;
 import io.github.noeppi_noeppi.libx.config.ConfigManager;
 import io.github.noeppi_noeppi.libx.mod.ModX;
@@ -39,7 +35,8 @@ public class JustEnoughAdvancements extends ModX {
         ConfigManager.registerConfig(this.modid, JeaConfig.class, false);
 
         FMLJavaModLoadingContext.get().getModEventBus().addListener(JeaRegistries::initRegistries);
-        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(CriterionSerializer.class, VanillaCriteriaSerializers::init);
+        
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(CriterionSerializer.class, VanillaPlugin::init);
 
         DistExecutor.unsafeRunForDist(() -> () -> {
             FMLJavaModLoadingContext.get().getModEventBus().addListener(SpecialModels::registerModels);
@@ -67,43 +64,6 @@ public class JustEnoughAdvancements extends ModX {
 
     @Override
     protected void clientSetup(FMLClientSetupEvent event) {
-        Jea.register(VanillaCriteriaIds.DESTROY_BEE_NEST, new DestroyBeeNestInfo());
-        Jea.register(VanillaCriteriaIds.BREED_ANIMALS, new BreedAnimalsInfo());
-        Jea.register(VanillaCriteriaIds.BREW_POTION, new BrewPotionInfo());
-        Jea.register(VanillaCriteriaIds.CHANGE_DIMENSION, new ChangeDimensionInfo());
-        Jea.register(VanillaCriteriaIds.CHANNELING_LIGHTNING, new ChannelingLightningInfo());
-        Jea.register(VanillaCriteriaIds.CONSTRUCT_BEACON, new ConstructBeaconInfo());
-        Jea.register(VanillaCriteriaIds.CONSUME_ITEM, new ConsumeItemInfo());
-        Jea.register(VanillaCriteriaIds.CURE_ZOMBIE_VILLAGER, new CureZombieVillagerInfo());
-        Jea.register(VanillaCriteriaIds.CHANGE_EFFECTS, new ChangeEffectsInfo());
-        Jea.register(VanillaCriteriaIds.ENCHANT_ITEM, new EnchantItemInfo());
-        Jea.register(VanillaCriteriaIds.ENTER_BLOCK, new EnterBlockInfo());
-        Jea.register(VanillaCriteriaIds.HURT_BY_ENTITY, new HurtByEntityInfo());
-        Jea.register(VanillaCriteriaIds.FILL_BUCKET, new FillBucketInfo());
-        Jea.register(VanillaCriteriaIds.FISHING_ROD_HOOK, new FishingRodHookInfo());
-        Jea.register(VanillaCriteriaIds.INVENTORY_CHANGE, new InventoryChangeInfo());
-        Jea.register(VanillaCriteriaIds.ITEM_DURABILITY, new ItemDurabilityInfo());
-        Jea.register(VanillaCriteriaIds.KILLED_BY_CROSSBOW, new KilledByCrossbowInfo());
-        Jea.register(VanillaCriteriaIds.LEVITATION, new LevitationInfo());
-        Jea.register(VanillaCriteriaIds.NETHER_TRAVEL, new NetherTravelInfo());
-        Jea.register(VanillaCriteriaIds.PLACE_BLOCK, new PlaceBlockInfo());
-        Jea.register(VanillaCriteriaIds.ENTITY_INTERACTION, new EntityInteractionInfo());
-        Jea.register(VanillaCriteriaIds.GENERATE_CONTAINER_LOOT, new GenerateContainerLootInfo());
-        Jea.register(VanillaCriteriaIds.LOCATION, new LocationInfo());
-        Jea.register(VanillaCriteriaIds.SLEEP_IN_BED, new SleepInBedInfo());
-        Jea.register(VanillaCriteriaIds.WIN_RAID, new WinRaidInfo());
-        Jea.register(VanillaCriteriaIds.RIGHT_CLICK_BLOCK, new RightClickBlockInfo());
-        Jea.register(VanillaCriteriaIds.SHOOT_CROSSBOW, new ShootCrossbowInfo());
-        Jea.register(VanillaCriteriaIds.SLIDE_BLOCK, new SlideBlockInfo());
-        Jea.register(VanillaCriteriaIds.SUMMON_ENTITY, new SummonEntityInfo());
-        Jea.register(VanillaCriteriaIds.TAME_ANIMAL, new TameAnimalInfo());
-        Jea.register(VanillaCriteriaIds.TARGET_HIT, new TargetHitInfo());
-        Jea.register(VanillaCriteriaIds.USE_TOTEM, new UseTotemInfo());
-        Jea.register(VanillaCriteriaIds.VILLAGER_TRADE, new VillagerTradeInfo());
-        Jea.register(VanillaCriteriaIds.HURT_ENTITY, new HurtEntityInfo());
-        Jea.register(VanillaCriteriaIds.KILLED_BY_ENTITY, new KilledByEntityInfo());
-        Jea.register(VanillaCriteriaIds.KILLED_ENTITY, new KilledEntityInfo());
-        
-        Jea.registerSpecial(RideStriderInfo.ADVANCEMENT, RideStriderInfo.CRITERION, new RideStriderInfo());
+        VanillaPlugin.register();
     }
 }
